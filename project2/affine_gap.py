f9e65f145e01cdcf29dbb275ebd1697668f894cd
LanderDurie <lander.durie@gmail.com>
2023-03-11 10:27:37 +0100

    bitvector reduce memory (but slow)

--- project2/affine_gap.py
+++ project2/affine_gap.py
@@ -1,5 +1,4 @@
-import time
-import gc
+import sys
 from Bio.SeqIO import parse
 from numpy import True_, bool_, zeros
 
@@ -56,78 +55,112 @@ BLOSUM = {
 
 
 def calc_matrix(row_seq: str, col_seq: str):
+
+    """
+    Calculate the score matrix and backtracking matrix.
+    The score matrix has been reduced to 2 columns to reduce memory. We can now constantly switch between these 2 columns.
+    During every position calculation step, The origin of the new value is stored in the backtracking (direction) matrix.
+    We can use this matrix to backtrack through both strings.
+    """
+
+    # pre calc sizes
     cols = len(col_seq)
     rows = len(row_seq)
+    total = cols * rows
 
-    score_matrix = [[[0 for _ in range(rows + 1)] for _ in range(3)] for _ in range(2)]
-    matrix_direction = [[zeros(cols + 1, dtype=bool_) for _ in range(rows + 1)] for _ in range(4)]
+    # create score and backtracking (direction) matrices
 
-    for row in range(1, rows + 1):
-        score_matrix[0][0][row] = -row - 10
-        score_matrix[0][1][row] = -row - 10
-        score_matrix[0][2][row] = -2147483647
+    score_cols_middle_new = [0]*(rows+1)
+    score_cols_lower_new = [0]*(rows+1)
+    score_cols_upper_new = [0]*(rows+1)
+    score_cols_middle_old = [0]*(rows+1)
+    score_cols_lower_old = [0]*(rows+1)
+    score_cols_upper_old = [0]*(rows+1)
 
-    score_matrix[0][1][0] = -2147483647
-    score_matrix[1][1][0] = -2147483647
+    matrix_direction_middle = zeros(total, dtype=bool_)
+    matrix_direction_lower = zeros(total, dtype=bool_)
+    matrix_direction_upper = zeros(total, dtype=bool_)
+    matrix_direction_spec = zeros(total, dtype=bool_)
+    # matrix_direction_middle = 0
+    # matrix_direction_lower = 0
+    # matrix_direction_upper = 0
+    # matrix_direction_spec = 0
 
-    indexes_options = [[1, 0], [0, 1]]
+    # initialize score matrix
+    for row in range(1, rows):
+        score_cols_middle_new[row] = score_cols_lower_old[row] = - row - 10
+        score_cols_upper_new[row] = -sys.maxsize
+    score_cols_lower_new[0] = score_cols_lower_old[0] = -sys.maxsize
 
     # fill matrix
-    for col in range(1, cols + 1):
-        indexes = indexes_options[col % 2]
+    # loop through cols
+    total_index = -1
+    for col in range(1, cols+1):
 
-        score_matrix[indexes[1]][0][0] = -col - 10
-        score_matrix[indexes[1]][2][0] = -col - 10
-
+        # switch current cols
+        score_cols_middle_old, score_cols_middle_new = score_cols_middle_new, score_cols_middle_old
+        score_cols_lower_old, score_cols_lower_new = score_cols_lower_new, score_cols_lower_old
+        score_cols_upper_old, score_cols_upper_new = score_cols_upper_new, score_cols_upper_old
+        # init new value
+        score_cols_middle_new[0] = score_cols_upper_new[0] = -col - 10
+        # get dict with first letter (smaller dict later on)
         first_letter_dict = BLOSUM[col_seq[col - 1]]
 
-        for row in range(1, rows + 1):
+        # loop through rows
+        for row in range(1, rows+1):
+            total_index += 1
 
-            middle_up, lower_up = score_matrix[indexes[1]][0][row - 1], score_matrix[indexes[1]][1][row - 1]
-
+            # calc new value for lower matrix
+            middle_up, lower_up = score_cols_middle_new[row - 1], score_cols_lower_new[row - 1]
             if middle_up < lower_up + 10:
                 lower_max = lower_up - 1
                 # set lower max index
-                matrix_direction[1][row][col] = True_
+                matrix_direction_lower[total_index] = True_
+                # matrix_direction_lower |= 1 << total_index
             else:
                 lower_max = middle_up - 11
 
-            middle_left, upper_left = score_matrix[indexes[0]][0][row], score_matrix[indexes[0]][2][row]
-
+            # calc new value for upper matrix
+            middle_left, upper_left = score_cols_middle_old[row], score_cols_upper_old[row]
             if middle_left < upper_left + 10:
                 upper_max = upper_left - 1
                 # set upper max index
-                matrix_direction[2][row][col] = True_
+                matrix_direction_upper[total_index] = True_
+                # matrix_direction_upper |= 1 << total_index
             else:
                 upper_max = middle_left - 11
 
             # calculate and assign middle matrix
-
             # follow middle cost
-            middle_max = score_matrix[indexes[0]][0][row - 1] + first_letter_dict.get(row_seq[row - 1])
-
+            middle_max = score_cols_middle_old[row - 1] + first_letter_dict.get(row_seq[row - 1])
             # compare lower and upper
             if lower_max < upper_max:
                 if middle_max < upper_max:
                     middle_max = upper_max
                     # set middle max index
-                    matrix_direction[0][row][col] = True_
+                    matrix_direction_middle[total_index] = True_
+                    # matrix_direction_middle |= 1 << total_index
                 # set lower / upper direction
-                matrix_direction[3][row][col] = True_
+                matrix_direction_spec[total_index] = True_
+                # matrix_direction_spec |= 1 << total_index
             else:
                 if middle_max < lower_max:
                     middle_max = lower_max
                     # set middle max index
-                    matrix_direction[0][row][col] = True_
+                    matrix_direction_middle[total_index] = True_
+                    # matrix_direction_middle |= 1 << total_index
 
-            score_matrix[indexes[1]][0][row], score_matrix[indexes[1]][1][row], score_matrix[indexes[1]][2][row] = middle_max, lower_max, upper_max
+            # assign values in score matrix
+            score_cols_middle_new[row], score_cols_lower_new[row], score_cols_upper_new[row] = middle_max, lower_max, upper_max
 
-    score = max((score_matrix[cols % 2][x][-1] for x in range(3)))
-
-    return score, matrix_direction
+    score = score_cols_middle_new[-1]
+    return score, [matrix_direction_middle, matrix_direction_lower, matrix_direction_upper, matrix_direction_spec]
 
 
 def load_file(filename: str):
+    """
+    Load both sequences from a fasta file using Bio.SeqIO
+    """
     data = list(parse(filename, "fasta"))
     first = str(data[0].seq)
     second = str(data[1].seq)
@@ -135,51 +168,53 @@ def load_file(filename: str):
 
 
 def global_alignment_score(filename: str):
-    gc.disable()
+    """
+    calculate the score matrix and return the score
+    """
     col_seq, row_seq = load_file(filename)
     score, _ = calc_matrix(row_seq, col_seq)
     return score
 
 
 def global_alignment(filename: str):
-    gc.disable()
+    """
+    calculate the score matrix and backtrack through the calculated matrix.
+    We can do this by looking at the value at a certain position and then backtracking to the previous one.
+    """
     col_seq, row_seq = load_file(filename)
     _, matrix_dir = calc_matrix(row_seq, col_seq)
 
-    row = len(row_seq)
-    col = len(col_seq)
+    rows = len(row_seq)
+    row = len(row_seq)-1
+    col = len(col_seq)-1
 
     # backtrack route
     row_align = ""
     col_align = ""
 
     current = 0
-
-    while row > 0 and col > 0:
-
-        direction = matrix_dir[current][row][col]
-
+    while row >= 0 and col >= 0:
+        index = rows * col + row
+        direction = matrix_dir[current][index]
         if current == 0:
             if direction:
-                if matrix_dir[3][row][col]:
+                if matrix_dir[3][index]:
                     current = 2
                 else:
                     current = 1
             else:
-                row_align = row_seq[row - 1] + row_align
-                col_align = col_seq[col - 1] + col_align
+                row_align = row_seq[row] + row_align
+                col_align = col_seq[col] + col_align
                 col -= 1
                 row -= 1
-
         elif current == 1:
-            row_align = row_seq[row - 1] + row_align
+            row_align = row_seq[row] + row_align
             col_align = '-' + col_align
             row -= 1
             if not direction:
                 current = 0
-
         else:
-            col_align = col_seq[col - 1] + col_align
+            col_align = col_seq[col] + col_align
             row_align = '-' + row_align
             col -= 1
             if not direction:
@@ -190,15 +225,16 @@ def global_alignment(filename: str):
         for i in reversed(range(0, row)):
             row_align = row_seq[i] + row_align
             col_align = '-' + col_align
+
     if col > 0:
         for i in reversed(range(0, col)):
             col_align = col_seq[i] + col_align
             row_align = '-' + row_align
-
     return col_align, row_align
 
 
 def affine_gap(filename: str):
+    import time
     file = load_file(filename)
     print(f"first:  {file[0]}")
     print(f"second: {file[1]}")
@@ -211,6 +247,7 @@ def affine_gap(filename: str):
 
 
 if __name__ == '__main__':
-    affine_gap("data/data_1000_benchmark.fna")
+    # affine_gap("data/data_1000_benchmark.fna")
     # affine_gap("data/data_19_affine.fna")
-    # affine_gap("data/data_01_affine.fna")
+    affine_gap("data/data_01_affine.fna")
+    # affine_gap("data/data_04_affine.fna")
